42900f369e1a244a4d0dfb2d7800beb0db768d9c
Diegoo11 <ynoacamino@gmail.com>
2022-04-24 12:21:18 -0500

    model's config

---
+++ models/book.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const { findByIdAndUpdate } = require('./somemodel');
+
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+    title: {type: String, required: true},
+    author: {type: Schema.Types.ObjectId, ref: 'Author', require: true},
+    summary: {type: String, required: true},
+    isbn: {type: String, required: true},
+    genre: [{type: Schema.Types.ObjectId, ref: 'Genre'}]
+});
+
+BookSchema
+.virtual('url')
+.get(function () {
+    return '/catalog/book/' + this._id;
+});
+
+module.exports = mongoose.model('Book', BookSchema);
